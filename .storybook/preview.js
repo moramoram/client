9992422ef3e60590cca20e9932f20b108c07a413
onshore <onshorecoding9@gmail.com>
2022-01-10 01:24:58 +0900

    FEAT : 기본 리엑트-스토리북 보일러플레이트 제작

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { GlobalStyle } from '../src/shared/global';
+
+// 모든 스토리에 스타일을 적용하기 위한 글로벌 decorator
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};

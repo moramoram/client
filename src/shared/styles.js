3eecc024efad143305cf1344cf5953c189f83e02
onshore <onshorecoding9@gmail.com>
2022-01-12 13:04:32 +0900

    Feat : 폰트 스타일 지정

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,28 +11,26 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#FF4785', // coral
-  secondary: '#1EA7FD', // ocean
-  tertiary: '#DDDDDD',
+  blue50: '#A5C1F7',
+  blue100: '#4A83EF', 
+  blue200: '#3669CD', 
 
-  orange: '#FC521F',
-  gold: '#FFAE00',
-  green: '#66BF3C',
-  seafoam: '#37D5D3',
-  purple: '#6F2CAC',
-  ultraviolet: '#2A0481',
-
+  
   // Monochrome
-  lightest: '#FFFFFF',
-  lighter: '#F8F8F8',
-  light: '#F3F3F3',
-  mediumlight: '#EEEEEE',
-  medium: '#DDDDDD',
-  mediumdark: '#999999',
-  dark: '#666666',
-  darker: '#444444',
-  darkest: '#333333',
+  white100: '#FFFFFF',
+  black100: '#212529',
 
+  gray25: '#F8F9FA',
+  gray50: '#F1F3F5',
+  gray100: '#E9ECEF',
+  gray200: '#DEE2E6',
+  gray300: '#CED4DA',
+  gray400: '#ADB5BD',
+  gray500: '#868E96',
+  gray600: '#4A5056',
+  gray700: '#3D4248',
+  gray800: '#31363B',
+  gray900: '#292D32',
   border: 'rgba(0,0,0,.1)',
 
   // Status
@@ -40,6 +38,11 @@ export const color = {
   negative: '#FF4400',
   warning: '#E69D00',
 };
+
+export const shadow = {
+  base: '0px 1px 3px rgba(0,0,0,0.1)',
+  button: '0px 1px 1px rgba(0,0,0,0.05)'
+}
 
 export const spacing = {
   padding: {
@@ -55,7 +58,7 @@ export const spacing = {
 
 export const typography = {
   type: {
-    primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+    primary: 'Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", sans-serif',
     code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
   },
   weight: {
@@ -65,15 +68,16 @@ export const typography = {
     black: '900',
   },
   size: {
-    s1: '12',
-    s2: '14',
-    s3: '16',
-    m1: '20',
-    m2: '24',
-    m3: '28',
-    l1: '32',
-    l2: '40',
-    l3: '48',
+    b1: '12',
+    b2: '14',
+
+    h1: '3rem',
+    h2: '2.25rem',
+    h3: '1.5rem',
+    h4: '1.25rem',
+    large: '1.125rem',
+    paragraph: '1rem',
+    small: '0.875rem',
     code: '90',
   },
 };
@@ -96,3 +100,4 @@ export const pageMargins = css`
     margin: 0 ${pageMargin * 4}%;
   }
 `;
+

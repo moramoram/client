70d4dff2f923ae1e90fafe5bd6c476386f10892a
onshore <onshorecoding9@gmail.com>
2022-01-12 13:21:16 +0900

    Chore : 애드온 docs 수정 및 Eslint 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,10 +5,10 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
   ],
   "framework": "@storybook/react",
   "core": {

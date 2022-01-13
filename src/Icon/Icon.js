499b75fde405848c9812db2061ebec7b2471982c
devpla <chaeyeonhee@kakao.com>
2022-01-13 15:51:16 +0900

    Refac : svg 속성값 코드 중복 제거

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -6,9 +6,12 @@ import { icons } from "../shared/icons";
 const Svg = styled.svg`
   display: ${(props) => (props.block ? "block" : "inline-block")};
   vertical-align: middle;
-
   shape-rendering: inherit;
   transform: translate3d(0, 0, 0);
+
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
 `;
 
 const Path = styled.path`

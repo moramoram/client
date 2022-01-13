7ccd0ae08816f704ddcb2ff09cf6085463bac83e
onshore <onshorecoding9@gmail.com>
2022-01-13 16:31:21 +0900

    Feat : 스토리북 배지 로딩 구현

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -1,46 +1,73 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
-import { color, typography } from "../shared/styles";
-import { Text } from "../Text/Text";
+import { color } from "../shared/styles";
+import { glowLight, glowDark } from "../shared/animation";
+
+const Text = styled.span`
+  display: inline-block;
+  vertical-align: top;
+`;
 
 const BadgeWrapper = styled.div`
   display: inline-block;
-  vertical-align: center;
+
   height: 20px;
   min-width: 48px;
-  line-height: 0.75rem;
   padding: 4px 8px;
   border-radius: 8px;
-  font-weight: ${typography.weight.regular};
+
+  text-align: center;
+  vertical-align: center;
+  line-height: 0.75rem;
   font-size: 0.625rem;
 
   svg {
-    height: 12px;
-    width: 12px;
+    height: 0.625rem;
+    width: 0.625rem;
     margin-right: 4px;
-    margin-top: -2px;
   }
+
+  animation: ${(props) =>
+    props.isLoading &&
+    props.isDarkmode &&
+    css`
+      ${glowDark} 1.5s ease-in-out infinite;
+    `};
+
+  animation: ${(props) =>
+    props.isLoading &&
+    !props.isDarkmode &&
+    css`
+      ${glowLight} 1.5s ease-in-out infinite;
+    `};
 
   ${(props) =>
     css`
-      color: ${color[props.color]};
       background: ${color[props.blackgroundColor]};
+      font-weight: ${props.isBold ? "700" : "400"};
+      color: ${color[props.color]};
     `};
 
   ${(props) =>
     props.type === TYPE.PRIMARY &&
     css`
-      color: ${color.white};
       background: ${color.blue100};
+      color: ${color.white};
     `};
 
   ${(props) =>
     props.type === TYPE.SECONDARY &&
     css`
+      background: ${props.isDarkmode ? color.gray800 : color.gray100};
       color: ${props.isDarkmode ? color.gray25 : color.gray600};
-      background: ${props.isDarkmode ? color.gray700 : color.gray200};
     `};
+
+  ${(props) =>
+    props.isLoading &&
+    `
+        background: ${props.isDarkmode ? color.gray800 : color.gray50};
+      `};
 `;
 
 const TYPE = {
@@ -51,10 +78,11 @@ const TYPE = {
 /**
  * **Badges?!** We don't need no stinkin' badges!!
  */
-export function Badge({ children, ...props }) {
+export function Badge({ children, isLoading, ...props }) {
+  const badgeinner = <>{isLoading ? <Text></Text> : <Text>{children}</Text>}</>;
   return (
-    <BadgeWrapper {...props}>
-      <Text>{children}</Text>
+    <BadgeWrapper isLoading={isLoading} {...props}>
+      {badgeinner}
     </BadgeWrapper>
   );
 }
@@ -63,6 +91,7 @@ Badge.propTypes = {
   type: PropTypes.oneOf(Object.values(TYPE)),
   blackgroundColor: PropTypes.string,
   color: PropTypes.string,
+  isBold: PropTypes.bool,
   isLoading: PropTypes.bool,
   isDarkmode: PropTypes.bool,
 };
@@ -71,6 +100,7 @@ Badge.defaultProps = {
   type: "primary",
   blackgroundColor: "blue200",
   color: "white",
+  isBold: false,
   isLoading: false,
   isDarkmode: false,
 };
--- src/Badge/Badge.stories.js
+++ src/Badge/Badge.stories.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Badge } from "../Badge/Badge";
 import { Icon } from "../Icon/Icon";
 
+export const Standard = (args) => <Badge {...args} />;
+Standard.args = {
+  children: "BADGE",
+};
+
 export default {
   title: "Design System/Badge",
   component: Badge,
@@ -9,22 +14,47 @@ export default {
 
 export const AllBadges = (args) => (
   <div>
+    <h4>Primary</h4>
     <Badge type="primary">BADGE</Badge>
+    <p />
+    <h4>Secondary</h4>
     <Badge type="secondary">BADGE</Badge>
+    <p />
+    <h4>Secondary Dark</h4>
     <Badge type="secondary" isDarkmode>
       BADGE
     </Badge>
+    <p />
+    <h4>Secondary Bold</h4>
+    <Badge type="secondary" weight="800">
+      BADGE
+    </Badge>
+    <p />
+    <h4>Custom</h4>
     <Badge blackgroundColor="blue200" color="white100">
       BADGE
     </Badge>
+    <p />
+    <h4>Loading Light</h4>
+    <Badge type="primary" isLoading>
+      BADGE
+    </Badge>
+    <p />
+    <h4>Loading Dark</h4>
+    <Badge type="primary" isLoading isDarkmode>
+      BADGE
+    </Badge>
+    <p />
+    <h4>With Icon</h4>
     <Badge status="positive">
       <Icon {...args} />
-      with icon
+      BADGE
     </Badge>
   </div>
 );
+
 AllBadges.args = {
-  icon: "facehappy",
+  icon: "smile",
   inline: true,
 };
 

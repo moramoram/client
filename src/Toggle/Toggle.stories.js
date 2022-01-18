22a98711d8ed18c9664d8848e8d7d15a4e6b2465
devpla <chaeyeonhee@kakao.com>
2022-01-18 22:31:08 +0900

    Feat : Toggle 다크모드 추가 및 개별 동작 구현

--- src/Toggle/Toggle.stories.js
+++ src/Toggle/Toggle.stories.js
@@ -1,9 +1,28 @@
 import React from "react";
+
 import { Toggle } from "./Toggle";
+import { Background } from "../Background/Background";
+import _ from "lodash";
 
 export default {
   title: "Design System/Toggle",
   component: Toggle,
 };
 
-export const Standard = (args) => <Toggle />;
+export const Default = (args) => (
+  <Background {...args}>
+    <Toggle {...args} />
+  </Background>
+);
+
+export const All = () => (
+  <>
+    <Background theme="light">
+      <Toggle theme="light" />
+    </Background>
+    <br />
+    <Background theme="dark">
+      <Toggle theme="dark" />
+    </Background>
+  </>
+);

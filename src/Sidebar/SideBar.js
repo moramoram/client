be7bff3cbd06a5566b0737e1b0369f706c947224
onshore <onshorecoding9@gmail.com>
2022-01-18 17:55:23 +0900

    Feat: 사이드바 로딩 기능 추가

--- src/Sidebar/SideBar.js
+++ src/Sidebar/SideBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Logo } from "../Logo/Logo";
 import styled, { css } from "styled-components";
 import { glowLight, glowDark } from "../shared/animation";
 import { Icon } from "../Icon/Icon";
@@ -14,10 +13,83 @@ const THEME = {
   DARK: "dark",
 };
 
-export const Sidebar = ({ theme, isLoading, name, src, ...props }) => {
-  let items = theme === THEME.LIGHT ? <Logo /> : <Logo theme="dark" />;
+export const Sidebar = ({
+  theme,
+  isLoading,
+  contents,
+  badges,
+  src,
+  ...props
+}) => {
+  if (isLoading) {
+    contents = [{}, {}, {}, {}];
+    badges = ["", "", ""];
+  }
+  return (
+    <SidebarWrapper theme={theme} isLoading={isLoading} {...props}>
+      <ImageBox className="thumbnail" theme={theme} isLoading={isLoading} />
+      <SidebarBox>
+        {contents.map((content, idx) => {
+          return (
+            <SidebarItem
+              className="contents-item"
+              key={idx}
+              theme={theme}
+              isLoading={isLoading}
+              contents={content}
+            />
+          );
+        })}
+      </SidebarBox>
+      <BadgeBox>
+        {badges.map((children, idx) => {
+          return (
+            <Badge
+              className="badge-item"
+              key={idx}
+              theme={theme}
+              isLoading={isLoading}
+              mode="secondary"
+              color="gray100"
+              isBold
+            >
+              {children}
+            </Badge>
+          );
+        })}
+      </BadgeBox>
 
-  const contents = [
+      <Button theme={theme} isLoading={isLoading} minWidth="380px">
+        <Icon icon="edit" />
+        지원하기
+      </Button>
+      <Button
+        theme={theme}
+        isLoading={isLoading}
+        mode="secondary"
+        minWidth="380px"
+      >
+        <Icon icon="bookmark" />
+        북마크 하기
+      </Button>
+    </SidebarWrapper>
+  );
+};
+
+Sidebar.propTypes = {
+  theme: PropTypes.oneOf(Object.values(THEME)),
+  isLoading: PropTypes.bool,
+  src: PropTypes.string,
+  contents: PropTypes.arrayOf(Object).isRequired,
+  badges: PropTypes.arrayOf(String).isRequired,
+};
+
+Sidebar.defaultProps = {
+  theme: THEME.LIGHT,
+  isLoading: false,
+  src: null,
+  name: "logo",
+  contents: [
     {
       title: "직무",
       description: "프론트엔드",
@@ -38,73 +110,8 @@ export const Sidebar = ({ theme, isLoading, name, src, ...props }) => {
       description: "서울 강남구",
       icon: "briefcase",
     },
-  ];
-
-  const badges = [
-    "JavaScript",
-    "TypeScript",
-    "Vue.js",
-    "React",
-    "Redux",
-    "Svelte",
-  ];
-
-  return (
-    <SidebarWrapper theme={theme} isLoading={isLoading} {...props}>
-      <ImageBox className="thumbnail" theme={theme} />
-      <SidebarBox>
-        {contents.map((content, id) => {
-          return (
-            <SidebarItem
-              className="contents-item"
-              key={id}
-              theme={theme}
-              contents={content}
-            />
-          );
-        })}
-      </SidebarBox>
-      <BadgeBox>
-        {badges.map((children, id) => {
-          return (
-            <Badge
-              className="badge-item"
-              key={id}
-              theme={theme}
-              mode="secondary"
-              color="gray100"
-              isBold
-            >
-              {children}
-            </Badge>
-          );
-        })}
-      </BadgeBox>
-
-      <Button theme={theme} minWidth="380px">
-        <Icon icon="edit" />
-        지원하기
-      </Button>
-      <Button theme={theme} mode="secondary" minWidth="380px">
-        <Icon icon="bookmark" />
-        북마크 하기
-      </Button>
-    </SidebarWrapper>
-  );
-};
-
-Sidebar.propTypes = {
-  theme: PropTypes.oneOf(Object.values(THEME)),
-  isLoading: PropTypes.bool,
-  src: PropTypes.string,
-  contents: PropTypes.objectOf(String).isRequired,
-};
-
-Sidebar.defaultProps = {
-  theme: THEME.LIGHT,
-  isLoading: false,
-  src: null,
-  name: "logo",
+  ],
+  badges: ["", "", "", ""],
 };
 
 const loadingAnimation = {
@@ -129,8 +136,6 @@ const SidebarWrapper = styled.div`
   border-radius: 16px;
   width: 400px;
   height: 540px;
-
-  animation: ${(props) => props.isLoading && loadingAnimation[props.theme]};
 
   button {
     margin: 5px 10px;

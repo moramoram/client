b9b310ad3582157b86343a69cce7fba5ae8f426e
onshore <onshorecoding9@gmail.com>
2022-01-21 15:47:14 +0900

    Feat : 라우팅 기본 설정 및 절대경로 설정

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import { RecoilRoot } from "recoil";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Router from "./router/Router";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Router />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </StrictMode>
+  );
+};
+
+export default App;

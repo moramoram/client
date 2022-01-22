087d532cebe788aa7c05b6dcb48a176e01d3dfff
devpla <chaeyeonhee@kakao.com>
2022-01-22 17:20:47 +0900

    Feat : Craco 도입 및 기본 설정

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "jsconfig",
+        jsConfigPath: "jsconfig.paths.json",
+      },
+    },
+  ],
+};

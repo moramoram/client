710b966a02572a9f5501458a9afff14da0604313
devpla <chaeyeonhee@kakao.com>
2022-01-23 22:50:26 +0900

    Design : Quill.js 에디터 css 커스텀

--- src/components/patterns/Editor/Editor.jsx
+++ src/components/patterns/Editor/Editor.jsx
@@ -44,7 +44,7 @@ const Editor = (props) => {
 
   return (
     <div style={editorStyle}>
-      <ReactQuill
+      <QuillEditor
         style={{ height: "600px" }}
         theme="snow"
         modules={modules}
@@ -64,3 +64,58 @@ const editorStyle = {
   width: "900px",
   height: "650px",
 };
+
+const QuillEditor = styled(ReactQuill)`
+  * {
+    font-family: Pretendard;
+    letter-spacing: -0.02rem;
+  }
+
+  .ql-container {
+    box-sizing: border-box;
+    font-size: 1rem;
+    height: 100%;
+    margin: 0px;
+    position: relative;
+    border-radius: 0 0 1rem 1rem;
+  }
+  .ql-editor {
+    box-sizing: border-box;
+    line-height: 1.42;
+    height: 100%;
+    outline: none;
+    overflow-y: auto;
+    padding: 1.25rem;
+    tab-size: 4;
+    -moz-tab-size: 4;
+    text-align: left;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+  .ql-snow .ql-editor h1 {
+    font-size: 2em;
+  }
+  .ql-snow .ql-editor h2 {
+    font-size: 1.5em;
+  }
+  .ql-snow .ql-editor h3 {
+    font-size: 1.25em;
+  }
+
+  .ql-toolbar.ql-snow {
+    border: 1px solid #ccc;
+    border-radius: 1rem 1rem 0 0;
+    background-color: #f8f9fa;
+    box-sizing: border-box;
+    font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    padding: 1rem;
+  }
+  .ql-snow .ql-tooltip {
+    background-color: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0px 0px 5px #ddd;
+    color: #444;
+    padding: 5px 12px;
+    white-space: nowrap;
+  }
+`;

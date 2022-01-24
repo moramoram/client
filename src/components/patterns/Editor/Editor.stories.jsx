42417f2a5291ea2bee5721748840172724f9c2a2
devpla <chaeyeonhee@kakao.com>
2022-01-24 17:54:11 +0900

    Docs : 스토리북 에디터 페이지 수정

--- src/components/patterns/Editor/Editor.stories.jsx
+++ src/components/patterns/Editor/Editor.stories.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-
 import Editor from "./Editor";
-// import { Background } from "@/foundations";
 
 export default {
   title: "Patterns/Editor",
   component: Editor,
 };
 
-export const Default = (args) => (
-  <>
-    <Editor {...args} />
-    <div></div>
-  </>
-);
-
-// export const AllTypes = () => (
-//   <>
-//     <Background theme="light">
-//       <CommentInput theme="light" />
-//     </Background>
-//     <Background theme="dark">
-//       <CommentInput theme="dark" />
-//     </Background>
-//   </>
-// );
+export const Default = (args) => <Editor {...args} />;

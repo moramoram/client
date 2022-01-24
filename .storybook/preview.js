08fdfca143504f3b6f8a0c91a960eb3fdc732e37
onshore <onshorecoding9@gmail.com>
2022-01-24 13:33:46 +0900

    Feat : 스토리북 react-router-dom 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+import {MemoryRouter} from 'react-router-dom';
 import { GlobalStyle } from "@/_shared/global";
 
 // 모든 스토리에 스타일을 적용하기 위한 글로벌 decorator
 export const decorators = [
   (Story) => (
-    <>
+    <MemoryRouter>
       <GlobalStyle />
       <Story />
-    </>
+    </MemoryRouter>
   ),
 ];
 

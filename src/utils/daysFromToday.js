22ed46c8476938614c65c0f7eda6bd1151a9b196
devpla <chaeyeonhee@kakao.com>
2022-01-25 12:04:33 +0900

    Feat : timedelta 유틸함수 추가

---
+++ src/utils/daysFromToday.js
@@ -0,0 +1,26 @@
+const daysFromToday = (value) => {
+  const today = new Date();
+  const timeValue = new Date(value);
+
+  const betweenTime = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60
+  );
+  if (betweenTime < 1) return "방금 전";
+  if (betweenTime < 60) {
+    return `${betweenTime}분 전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간 전`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 365) {
+    return `${betweenTimeDay}일 전`;
+  }
+
+  return `${Math.floor(betweenTimeDay / 365)}년 전`;
+};
+
+export default daysFromToday;

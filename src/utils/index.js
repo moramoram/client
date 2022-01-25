7d1dc846fc0ef20c3fc3edf827459964b83c243f
devpla <chaeyeonhee@kakao.com>
2022-01-25 15:51:46 +0900

    Feat : 숫자 변환 유틸 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,2 @@
 export { default as daysFromToday } from "./daysFromToday";
+export { default as numToMillion } from "./numToMillion";
---
+++ src/utils/numToMillion.js
@@ -0,0 +1,7 @@
+const numToMillion = (num) => {
+  if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`;
+  if (num >= 1000) return `${(num / 1000).toFixed(1)}K`;
+  return num;
+};
+
+export default numToMillion;

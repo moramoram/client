aff75bb20780cf6efeac400634e4184166f79762
devpla <chaeyeonhee@kakao.com>
2022-01-27 15:20:45 +0900

    Feat : 반응형 카드 그리드 컨테이너 추가

---
+++ src/containers/commons/CardResponsiveGrid/CardResponsiveGrid.stories.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+import CardResponsiveGrid from ".";
+import { colors } from "@/_shared";
+
+export default {
+  title: "containers/CardResponsiveGrid",
+  component: CardResponsiveGrid,
+};
+
+export const Default = (args) => <CardResponsiveGrid {...args} />;
+
+Default.args = {
+  theme: "light",
+  data: [
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+    {
+      contents: {
+        title: "주니어 프론트엔드 채용",
+        subtitle: "싸페 디자인 시스템",
+        highlight: "D-day",
+        src: "",
+      },
+      badges: ["JavaScript", "React", "Vue.js"],
+      url: "",
+    },
+  ],
+};

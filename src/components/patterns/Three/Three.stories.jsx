9cd5aee372c948c70acf89cfcdc4d914eded3199
devpla <chaeyeonhee@kakao.com>
2022-01-29 00:46:15 +0900

    Feat : Three.js 노트북 컴포넌트 추가

---
+++ src/components/patterns/Three/Three.stories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Three from "./";
+
+export default {
+  title: "Patterns/Three",
+  component: Three,
+};
+
+export const Default = () => (
+  <>
+    <Three />
+  </>
+);

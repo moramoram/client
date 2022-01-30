c24685ac273834c3f011a81e38b91df6e7b829ba
devpla <chaeyeonhee@kakao.com>
2022-01-30 22:26:08 +0900

    Refac : 불필요한 코드 제거

--- src/containers/JobPage/JobDetailMobile/JobDetailMobile.jsx
+++ src/containers/JobPage/JobDetailMobile/JobDetailMobile.jsx
@@ -160,46 +160,6 @@ const summaryData = [
   },
 ];
 
-const contentData = (
-  <>
-    <h3>주요업무</h3>
-    <ul>
-      <li>React를 활용한 서비스 개발</li>
-    </ul>
-    <p>
-      <br />
-    </p>
-    <h3>자격요건</h3>
-    <ul>
-      <li>학력: 초대졸 이상</li>
-      <li>경력: 3년 이상</li>
-      <li>React 등을 활용한 3년 이상 혹은 그에 준하는 개발 능력을 보유한 분</li>
-    </ul>
-    <p>
-      <br />
-    </p>
-    <h3>우대사항</h3>
-    <ul>
-      <li>React 등을 활용한 서비스 개발 및 배포 경험 보유한 분</li>
-      <li>HTML, CSS, JavaScript에 대한 이해도 보유한 분</li>
-      <li>다양한 문제에 대해 체계적인 원인 분석 및 해결 능력을 보유</li>
-    </ul>
-    <p>
-      <br />
-    </p>
-    <h3>혜택 및 복지</h3>
-    <ul>
-      <li>수평적인 사내문화와 자유로운 분위기</li>
-      <li>자율 복장, 유연근무제</li>
-      <li>
-        경조금(휴가) 및 연차, 코로나 백신휴가, 건강검진/예비군 훈련 시 공가휴가
-        제공(연차에서 차감 안됨), 조식제공
-      </li>
-      <li>워크샵, 책모임, 스터디</li>
-    </ul>
-  </>
-);
-
 const titleColor = {
   light: colors.gray900,
   dark: colors.gray25,

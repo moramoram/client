0a5965543f86cf5c0288b23e8d4e0ea1390ea078
onshore <onshorecoding9@gmail.com>
2022-01-30 23:26:59 +0900

    Refac : 불필요한 공백 제거

--- src/pages/JobPage/JobDetailPage.jsx
+++ src/pages/JobPage/JobDetailPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { themeState } from "@/recoil/theme";

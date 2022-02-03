fa3661b511843b7244cfad6ad3a313b1db415d25
devpla <chaeyeonhee@kakao.com>
2022-02-03 23:33:30 +0900

    Feat : 내가 쓴 댓글 페이지 레이아웃 작성

--- src/containers/MyPage/MyComment/MyComment.jsx
+++ src/containers/MyPage/MyComment/MyComment.jsx
@@ -2,18 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { useRecoilValue } from "recoil";
+import { themeState } from "@/recoil/theme";
+
+import { CommentList } from "@/containers";
 import { colors, fontSize, lineHeight, fontWeight } from "@/_shared";
+
+import { daysFromToday } from "@/utils";
 
 const THEME = {
   LIGHT: "light",
   DARK: "dark",
 };
 
-const MyComment = ({ ...props }) => {
+const MyComment = () => {
+  const theme = useRecoilValue(themeState);
+
   return (
     <Layout>
-      <Title {...props}>내가 쓴 댓글</Title>
-      <SubTitle {...props}>내가 쓴 댓글을 확인하세요.</SubTitle>
+      <Title theme={theme}>내가 쓴 댓글</Title>
+      <SubTitle theme={theme}>
+        지금까지 총 {commentData.length}개의 댓글을 남겼어요
+      </SubTitle>
+      {/* TODO : 원본 글 링크 */}
+      <CommentList data={commentData} theme={theme} />
     </Layout>
   );
 };
@@ -29,6 +41,58 @@ MyComment.defaultProps = {
 
 export default MyComment;
 
+const commentData = [
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2022-01-31"),
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in lorem ut sapien placerat vulputate.",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2022-01-30"),
+    content:
+      "Nam tempus, est id rutrum suscipit, metus mi tincidunt nulla, ut rutrum magna tortor non velit. Suspendisse gravida pretium porta. Praesent eget vestibulum mauris. Nullam aliquet enim felis, in iaculis purus tempus pharetra.",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2022-01-24"),
+    content:
+      "Aliquam et lectus ultricies, fringilla ipsum ac, vulputate sapien.",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2022-01-12"),
+    content:
+      "Integer nulla sem, eleifend non ex id, aliquam tempor tellus. Sed vehicula justo ut diam semper mollis.",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2021-12-10"),
+    content:
+      "Donec nec tristique arcu. Curabitur at facilisis nibh. Mauris vel nisi ipsum. Morbi vitae sapien metus. Ut et quam a erat rutrum maximus. Vestibulum non elementum enim. ",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2021-07-10"),
+    content:
+      "Ut in bibendum metus. Duis sed egestas ante. Etiam ex tortor, vehicula ac mollis sit amet, dapibus eget urna.",
+  },
+  {
+    username: null,
+    src: null,
+    created: daysFromToday("2020-07-10"),
+    content:
+      "Nam tempus, est id rutrum suscipit, metus mi tincidunt nulla, ut rutrum magna tortor non velit. Suspendisse gravida pretium porta. Praesent eget vestibulum mauris. Nullam aliquet enim felis, in iaculis purus tempus pharetra.",
+  },
+];
+
 const titleColor = {
   light: colors.gray900,
   dark: colors.gray25,
@@ -41,15 +105,8 @@ const subtitleColor = {
 
 const Layout = styled.div`
   display: flex;
-
-  width: 100vw;
-  height: 320px;
-
   flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  padding-top: 86px;
 `;
 
 const Title = styled.div`
@@ -62,8 +119,7 @@ const Title = styled.div`
 `;
 
 const SubTitle = styled.div`
-  height: ${lineHeight.h4};
-  margin-bottom: 27px;
+  /* margin-bottom: 3rem; */
 
   font-size: ${fontSize.p};
   color: ${(props) => subtitleColor[props.theme]};

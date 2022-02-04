1ffd55e7f9368830e9c289db681b75956393182a
devpla <chaeyeonhee@kakao.com>
2022-02-04 19:13:37 +0900

    Fix : Console 오류 해결

--- src/containers/CommunityPage/CommunityIntro/CommunityIntro.jsx
+++ src/containers/CommunityPage/CommunityIntro/CommunityIntro.jsx
@@ -16,7 +16,7 @@ const CommunityIntro = ({ ...props }) => {
       <Layout {...props}>
         <Title {...props}>커뮤니티</Title>
         <SubTitle {...props}>싸피인들끼리 자유롭게 소통하세요</SubTitle>
-        <Button onClick={props.onButtonClick}>게시글 작성하기</Button>
+        <Button onClick={props.handleButtonClick}>게시글 작성하기</Button>
       </Layout>
     </>
   );

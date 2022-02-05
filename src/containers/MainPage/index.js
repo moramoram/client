a08739e48b7ff7880a45939d9b35b5e1d84012c6
devpla <chaeyeonhee@kakao.com>
2022-02-05 23:53:41 +0900

    Feat : MainIntroSlider 컨테이너 추가

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -1,2 +1,3 @@
+export { default as MainIntroSlider } from "./MainIntroSlider";
 export { default as MainJobSlider } from "./MainJobSlider";
 export { default as MainStudySilder } from "./MainStudySilder";

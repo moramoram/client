1dfc603a34984c4cd609e4a4f73fb4633bf025c8
onshore <onshorecoding9@gmail.com>
2022-02-05 20:43:09 +0900

    Feat : 다크모드 recoil effects_UNSTABLE 적용

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ export { default as daysFromToday } from "./daysFromToday";
 export { default as daysLeftFromToday } from "./daysLeftFromToday";
 export { default as numToMillion } from "./numToMillion";
 export { default as getCroppedImg } from "./getCroppedImg";
+export * from "./localStorageEffect";
 export { default as axiosInstance } from "./axiosConfig";

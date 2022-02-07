cc76f3044dc5510b714997fbfc7ff7a99197d5aa
devpla <chaeyeonhee@kakao.com>
2022-02-07 14:22:52 +0900

    Feat : 별명 Input Validation 추가

--- src/containers/MyPage/MyInfo/MyInfo.jsx
+++ src/containers/MyPage/MyInfo/MyInfo.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import { useRecoilValue } from "recoil";
 import { themeState } from "@/recoil/theme";
 
+import axios from "axios";
 import { Input, Button, AvatarUploader } from "@/components";
 import { colors, fontSize, lineHeight, fontWeight } from "@/_shared";
 
@@ -14,8 +15,65 @@ const THEME = {
 };
 
 const MyInfo = ({ ...props }) => {
-  const [confirmUsername, setConfirmUsername] = useState(false);
   const theme = useRecoilValue(themeState);
+  // TODO : 프로필 State에서 별명 가져오기
+  const nickname = "별명";
+
+  const [nickValue, setNickValue] = useState(nickname);
+  const [nickStatus, setNickStatus] = useState({
+    status: "default",
+    message: null,
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+
+    if (value.length > 8) {
+      e.target.value = value.slice(0, 9);
+    }
+    setNickValue(value);
+    validateNickname(value);
+  };
+
+  const validateNickname = useCallback((value) => {
+    const pattern = /([^가-힣a-zA-Z0-9])/;
+
+    if (value.length < 2 || value.length > 8) {
+      setNickStatus({
+        status: "error",
+        message: "별명을 2글자 이상 8글자 이하로 입력해주세요",
+      });
+    } else if (pattern.test(value)) {
+      setNickStatus({
+        status: "error",
+        message: "한글, 영문, 혹은 숫자를 올바르게 입력해주세요",
+      });
+    } else setNickStatus({ status: "default", message: null });
+  });
+
+  const getNicknameCheck = useCallback(async () => {
+    const res = await axios.get(
+      "https://my-json-server.typicode.com/moramoram/mockup-api/nickname-check/"
+    );
+    if (res.data) {
+      setNickStatus({
+        status: "error",
+        message: "이미 사용중인 별명이에요. 다른 별명으로 지어주세요.",
+      });
+      console.log(res);
+    } else {
+      nickStatus({ status: "default", message: null });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (nickStatus.status === "default" && nickname !== nickValue) {
+      const timeoutId = setTimeout(() => {
+        getNicknameCheck();
+      }, 500);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [nickValue]);
 
   return (
     <Layout>
@@ -27,24 +85,24 @@ const MyInfo = ({ ...props }) => {
         <LabelBox>
           <Label theme={theme}>별명</Label>
           <InputBox>
-            {/* TODO : 프로필 State에서 별명 가져오기 */}
             <Input
               placeholder="별명을 입력하세요"
-              status={!confirmUsername ? "default" : "error"}
-              // TODO: Form Validation
-              message={
-                confirmUsername
-                  ? "이미 사용중인 별명이에요. 다른 별명으로 지어주세요."
-                  : null
-              }
+              onChange={handleChange}
+              value={nickValue}
+              status={nickStatus.status}
+              message={nickStatus.message}
               theme={theme}
             />
             <Button
               mode="active"
-              onClick={() => setConfirmUsername(!confirmUsername)}
+              disabled={
+                nickStatus.status === "error" || nickValue === nickname
+                  ? true
+                  : false
+              }
               theme={theme}
             >
-              중복 확인
+              변경하기
             </Button>
           </InputBox>
         </LabelBox>
@@ -78,7 +136,7 @@ MyInfo.defaultProps = {
 
 export default MyInfo;
 
-const titleColor = {
+const textColor = {
   light: colors.gray900,
   dark: colors.gray25,
 };
@@ -86,11 +144,6 @@ const titleColor = {
 const subtitleColor = {
   light: colors.gray400,
   dark: colors.gray500,
-};
-
-const labelColor = {
-  light: colors.gray900,
-  dark: colors.gray25,
 };
 
 const Layout = styled.div`
@@ -105,7 +158,7 @@ const Title = styled.div`
 
   font-weight: ${fontWeight.bold};
   font-size: ${fontSize.h3};
-  color: ${(props) => titleColor[props.theme]};
+  color: ${(props) => textColor[props.theme]};
 `;
 
 const SubTitle = styled.div`
@@ -147,11 +200,13 @@ const LabelBox = styled.div`
 const Label = styled.div`
   font-size: ${fontSize.sm};
   font-weight: ${fontWeight.bold};
-  color: ${(props) => labelColor[props.theme]};
+  color: ${(props) => textColor[props.theme]};
 `;
 
 const AvatarBox = styled.div`
-  padding-top: 6px;
+  display: flex;
+  gap: 1rem;
+  padding-top: 12px;
 `;
 
 const ButtonBox = styled.div`

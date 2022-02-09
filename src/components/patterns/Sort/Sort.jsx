6c395146e5e9fdea79813676b068dc54e7cb3200
devpla <chaeyeonhee@kakao.com>
2022-02-09 19:39:31 +0900

    Refac : 불필요한 코드 제거

--- src/components/patterns/Sort/Sort.jsx
+++ src/components/patterns/Sort/Sort.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import { DropdownSmall } from "@/components";
 import { Icon } from "@/foundations";
-import { colors, fontSize, fontWeight, animations } from "@/_shared";
+import { colors, fontSize, animations } from "@/_shared";
 
 const THEME = {
   LIGHT: "light",

dc34dbb6d7ab49812d75f2e258052908de67027a
devpla <chaeyeonhee@kakao.com>
2022-02-09 21:25:05 +0900

    Feat : CardSmallSlider 컨테이너 추가

--- src/containers/commons/index.js
+++ src/containers/commons/index.js
@@ -1,6 +1,7 @@
 export { default as CardGrid } from "./CardGrid";
-export { default as CardSmallGrid } from "./CardSmallGrid";
 export { default as CardSlider } from "./CardSlider";
+export { default as CardSmallGrid } from "./CardSmallGrid";
+export { default as CardSmallSlider } from "./CardSmallSlider";
 export { default as CommentList } from "./CommentList";
 export { default as FeedGrid } from "./FeedGrid";
 export { default as FeedSmallGrid } from "./FeedSmallGrid";

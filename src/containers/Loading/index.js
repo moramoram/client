a2880bfeccf59a0446ad61ec8932d1fba1267ba8
devpla <chaeyeonhee@kakao.com>
2022-02-10 21:55:27 +0900

    Feat : MyPage 로딩 Suspense 추가

--- src/containers/Loading/index.js
+++ src/containers/Loading/index.js
@@ -1,3 +1,4 @@
 export { default as LoadingDetail } from "./LoadingDetail";
 export { default as LoadingDetailMobile } from "./LoadingDetailMobile";
 export { default as LoadingCommunityDetail } from "./LoadingCommunityDetail";
+export { default as LoadingMyPages } from "./LoadingMyPages";

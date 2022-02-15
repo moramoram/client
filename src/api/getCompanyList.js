f0cb0a5777fc83ea8ca72acf8f24291b0a4e4359
onshore <onshorecoding9@gmail.com>
2022-02-15 10:14:00 +0900

    Feat : 기업리스트 기능 추가

--- src/api/getCompanyList.js
+++ src/api/getCompanyList.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { axiosInstance } from "@/utils";
 
 export const GetCompanyList = () =>
-  useQuery("getCompanyList", () => fetchData());
+  useQuery("getCompanyList", () => fetchData(), { suspense: false });
 
 const fetchData = async () => {
   const res = await axiosInstance({

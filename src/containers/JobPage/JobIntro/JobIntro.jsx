59a30e712bd856e71933f38e5ba259607b60e44a
devpla <chaeyeonhee@kakao.com>
2022-02-16 22:22:07 +0900

    Feat : JobPage 및 StudyPage Intro 이미지 public에 추가

--- src/containers/JobPage/JobIntro/JobIntro.jsx
+++ src/containers/JobPage/JobIntro/JobIntro.jsx
@@ -47,7 +47,7 @@ const Layout = styled.div`
 
   width: 100%;
   height: 400px;
-  background: url("https://i.imgur.com/3QTKlkH.jpg");
+  background: url("/images/job-intro.png");
   background-repeat: no-repeat;
   background-size: cover;
 

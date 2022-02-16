af496efe0c3e379c819ae4c0ca21b6182d2b7f09
devpla <chaeyeonhee@kakao.com>
2022-02-17 01:16:04 +0900

    Feat : AuthWait 페이지 레이아웃 변경

--- src/containers/MyPage/AuthWait/AuthWait.jsx
+++ src/containers/MyPage/AuthWait/AuthWait.jsx
@@ -1,91 +1,96 @@
 import React from "react";
 import styled from "styled-components";
 
+import { Icon, Logo, Typography } from "@/foundations";
 import { Button } from "@/components";
-import { Logo } from "@/foundations";
-import { colors, fontSize, lineHeight, fontWeight } from "@/_shared";
+import { colors, fontWeight } from "@/_shared";
+import { useSetRecoilState } from "recoil";
+import { modalState } from "@/recoil";
 
 const AuthWait = ({ ...props }) => {
-  return (
-    <Layout>
-      <LogoBox>
-        <Logo width="80" height="20" {...props} />
-      </LogoBox>
-      <Title {...props}>아직 인증이 완료되지 않았어요</Title>
-      <SubTitle {...props}>
-        {`관리자가 열심히 확인중이에요. \n 조금만 더 기다려주세요!`}
-      </SubTitle>
+  const setIsModalOpened = useSetRecoilState(modalState);
 
-      <ButtonLink href="/main">
-        <Button>메인으로 돌아가기</Button>
-      </ButtonLink>
-      <Link
-        href="https://github.com/moramoram/ssafe-service/discussions/categories/q-a"
-        target="_blank"
-      >
-        <AskingForHelp>도움이 필요하신가요?</AskingForHelp>
-      </Link>
-    </Layout>
+  return (
+    <>
+      <CloseIconBox>
+        <Icon icon="x" onClick={() => setIsModalOpened(false)} />
+      </CloseIconBox>
+      <ContentBox>
+        <LogoBox>
+          <Logo width="80" height="20" />
+        </LogoBox>
+        <TextBox>
+          <Title type="h2">아직 인증이 완료되지 않았어요.</Title>
+          <SubTitle type="paragraph">
+            {`관리자가 열심히 확인중이에요. \n 조금만 더 기다려주세요!`}
+          </SubTitle>
+        </TextBox>
+        <ButtonLink href="/main">
+          <Button>메인으로 돌아가기</Button>
+        </ButtonLink>
+        <Link
+          href="https://github.com/moramoram/ssafe-service/discussions/categories/q-a"
+          target="_blank"
+        >
+          <AskingForHelp>도움이 필요하신가요?</AskingForHelp>
+        </Link>
+      </ContentBox>
+    </>
   );
 };
 
 export default AuthWait;
 
-const titleColor = {
-  light: colors.gray900,
-  dark: colors.gray25,
-};
+const LogoBox = styled.div``;
 
-const subtitleColor = {
-  light: colors.gray400,
-  dark: colors.gray500,
-};
+const CloseIconBox = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 1rem;
 
-const Layout = styled.div`
+  svg {
+    width: 18px;
+    stroke: ${colors.gray500};
+    cursor: pointer;
+  }
+`;
+
+const ContentBox = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  padding-top: 86px;
+  align-items: flex-start;
+  margin: 40px;
+  gap: 30px;
 `;
 
-const LogoBox = styled.div`
-  padding-bottom: 1rem;
+const TextBox = styled.div`
+  margin-bottom: 40px;
 `;
 
-const Title = styled.div`
-  line-height: ${lineHeight.h2};
-  margin-bottom: 2rem;
-
-  font-weight: ${fontWeight.bold};
-  font-size: ${fontSize.h3};
-  color: ${(props) => titleColor[props.theme]};
+const Title = styled(Typography)`
+  color: ${colors.gray900};
+  text-align: left;
+  white-space: pre-line;
+  margin-bottom: 28px;
 `;
 
-const SubTitle = styled.div`
-  margin-bottom: 6rem;
-
-  color: ${(props) => subtitleColor[props.theme]};
-  font-size: ${fontSize.p};
-  line-height: ${lineHeight.p};
-
+const SubTitle = styled(Typography)`
+  color: ${colors.gray400};
+  text-align: left;
   white-space: pre-line;
 `;
 
 const ButtonLink = styled.a`
   text-decoration: none;
-  margin-bottom: 2rem;
+`;
+
+const AskingForHelp = styled.div`
+  color: ${colors.blue100};
+  font-weight: ${fontWeight.bold};
+  cursor: pointer;
 `;
 
 const Link = styled.a`
   text-decoration: none;
-`;
-
-const AskingForHelp = styled.span`
-  color: ${colors.blue100};
-  font-weight: ${fontWeight.bold};
-  cursor: pointer;
-
-  :hover {
-    color: ${colors.blue200};
-  }
 `;

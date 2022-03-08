920aa5386447130ec30a5fd0131a9f572c1746e4
onshore <onshorecoding9@gmail.com>
2022-03-09 08:04:19 +0900

    Fix : 그리드 레이아웃 오류 수정

--- src/components/layouts/CardGrid/CardGrid.styled.js
+++ src/components/layouts/CardGrid/CardGrid.styled.js
@@ -5,7 +5,7 @@ export const Layout = styled.section`
   display: grid;
   width: 100%;
   max-width: 940px;
-  grid-template-columns: repeat(auto-fill, minmax(300px, auto));
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 20px;
 `;
 
